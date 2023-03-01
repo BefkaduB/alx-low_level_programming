da30274d7283854ab4b56711bd7029f0c7b0f9d6
BefkaduB <frewbeletebefkadu@gmail.om>
2023-02-28 16:21:44 -0800

    fix on 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_rev - prints a string in revers
  * @s: string to be printed
@@ -7,7 +8,7 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\n')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
