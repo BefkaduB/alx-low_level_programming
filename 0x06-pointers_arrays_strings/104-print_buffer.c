f5d25637d85b92230d27cac7b196fa2d15aeb333
BefkaduB <frewbeletebefkadu@gmail.om>
2023-03-02 15:27:35 -0800

    task 104-print_buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -16,9 +16,10 @@ void print_buffer(char *b, int size)
 		printf("\n");
 		return;
 	}
-	while (0 < size)
+	while (o < size)
 	{
 		j = size - o < 10 ? size - o : 10;
+
 		printf("%08x:", o);
 		for (i = 0; i < 10; i++)
 		{
@@ -34,6 +35,7 @@ void print_buffer(char *b, int size)
 		for (i = 0; i < j; i++)
 		{
 			int c = *(b + o + i);
+
 			if (c < 32 || c > 132)
 			{
 				c = '.';
