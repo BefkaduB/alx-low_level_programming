f0a82b5dae9d3ab9d9fe47176d459dbe5f8b54cc
BefkaduB <frewbeletebefkadu@gmail.om>
2023-04-05 17:09:02 -0700

    fix

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,10 +9,10 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 	int num;
 
-	if (!head || *head)
+	if (!head || !*head)
 		return (0);
 
-	num = (*head)->next;
+	num = (*head)->n;
 	temp = (*head)->next;
 	free(*head);
 	*head = temp;
